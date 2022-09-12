9b5cc4c27f36f56a34ea4193734294d96cbb1350
rpeveri <ricardopeveri@gmail.com>
2022-09-12 20:05:00 -0300

    Add files via upload

---
+++ cargar_dashicons.css.js
@@ -0,0 +1,7 @@
+(function() {
+    var cssMain = document.createElement('link');
+    cssMain.href = 'https://scripts-fast.pages.dev/dashicons.css';
+    cssMain.rel = 'stylesheet';
+    cssMain.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(cssMain);
+})();
