ca26c77e00d040b8c6f41de652c29af42207f6cf
rpeveri <ricardopeveri@gmail.com>
2022-09-13 07:58:31 -0300

    Add files via upload

---
+++ cargar_estilosgenesisplayonline.js
@@ -0,0 +1,7 @@
+(function() {
+    var cssMain = document.createElement('link');
+    cssMain.href = 'https://scripts-fast.pages.dev/estilosgenesisplayonline.css';
+    cssMain.rel = 'stylesheet';
+    cssMain.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(cssMain);
+})();
